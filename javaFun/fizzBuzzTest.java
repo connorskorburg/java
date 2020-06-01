575c34ae7197d5a26e42a63d9c5788140f3fcc41
Connor <connorskorburg@gmail.com>
2020-06-01 14:21:08 -0500

    added fizzbuzz algo

---
+++ javaFun/fizzBuzzTest.java
@@ -0,0 +1,24 @@
+public class fizzBuzzTest {
+    public static void main(String[] args ) {
+        //try 1
+        fizzBuzz try1 = new fizzBuzz(); 
+        int val1 = 30;
+        String result1 = try1.fizzBuzz(val1);
+        System.out.println(result1);
+        //try 2
+        fizzBuzz try2 = new fizzBuzz(); 
+        int val2 = 10;
+        String result2 = try2.fizzBuzz(val2);
+        System.out.println(result2);
+        //try 3
+        fizzBuzz try3 = new fizzBuzz(); 
+        int val3 = 9;
+        String result3 = try3.fizzBuzz(val3);
+        System.out.println(result3);
+        //try 2
+        fizzBuzz try4 = new fizzBuzz(); 
+        int val4 = 2;
+        String result4 = try4.fizzBuzz(val4);
+        System.out.println(result4);
+    }
+} 
