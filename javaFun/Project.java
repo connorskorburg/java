a87c523ea69a9d42b27d361d0e9b9f5ded27867c
Connor <connorskorburg@gmail.com>
2020-06-03 17:39:59 -0500

    init project practice file

---
+++ javaFun/Project.java
@@ -0,0 +1,8 @@
+public class Project {
+    private String name;
+    private String decsription;
+    
+    public String elevatorPitch() {
+        // return "%s : %s".format(name, decsription);
+    }
+}
