c209d94d374a8c38af5de3cb340ec599469478a5
Connor <connorskorburg@gmail.com>
2020-06-15 10:48:21 -0500

    initial commit for springData folder

---
+++ springData/UpdateDeleteProject/src/main/java/com/connorskorburg/updatedelete/UpdateDeleteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.connorskorburg.updatedelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateDeleteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateDeleteProjectApplication.class, args);
+	}
+
+}
---
+++ springData/UpdateDeleteProject/src/test/java/com/connorskorburg/updatedelete/UpdateDeleteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connorskorburg.updatedelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateDeleteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
