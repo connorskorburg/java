baca4d9ccafe1dc727bed8803ebb08f6e0072b04
Connor <connorskorburg@gmail.com>
2020-06-19 12:32:40 -0500

    added show project

---
+++ springData/ShowProject/src/test/java/com/connorskorburg/showproject/ShowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connorskorburg.showproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
